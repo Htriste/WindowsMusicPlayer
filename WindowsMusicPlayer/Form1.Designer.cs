a2670aab5d8e72e55169f8e00d4c0439c8c870b0
Htriste <130944089+Htriste@users.noreply.github.com>
2024-05-09 10:30:05 +0800

    Add files via upload

---
+++ WindowsMusicPlayer/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace WindowsMusicPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip9 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.selectNormalFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.selectOggFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.contextMenuStrip9.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(494, 345);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "选择音乐文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // contextMenuStrip9
+            // 
+            this.contextMenuStrip9.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip9.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.selectNormalFilesToolStripMenuItem,
+            this.selectOggFilesToolStripMenuItem});
+            this.contextMenuStrip9.Name = "contextMenuStrip9";
+            this.contextMenuStrip9.Size = new System.Drawing.Size(228, 64);
+            // 
+            // selectNormalFilesToolStripMenuItem
+            // 
+            this.selectNormalFilesToolStripMenuItem.Name = "selectNormalFilesToolStripMenuItem";
+            this.selectNormalFilesToolStripMenuItem.Size = new System.Drawing.Size(227, 30);
+            this.selectNormalFilesToolStripMenuItem.Text = "选择普通音乐文件";
+            this.selectNormalFilesToolStripMenuItem.Click += new System.EventHandler(this.selectNormalFilesToolStripMenuItem_Click);
+            // 
+            // selectOggFilesToolStripMenuItem
+            // 
+            this.selectOggFilesToolStripMenuItem.Name = "selectOggFilesToolStripMenuItem";
+            this.selectOggFilesToolStripMenuItem.Size = new System.Drawing.Size(227, 30);
+            this.selectOggFilesToolStripMenuItem.Text = "选择.ogg音乐文件";
+            this.selectOggFilesToolStripMenuItem.Click += new System.EventHandler(this.selectOggFilesToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(109, 150);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(319, 166);
+            this.listBox1.TabIndex = 11;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.trackBar1.Location = new System.Drawing.Point(667, 150);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(69, 150);
+            this.trackBar1.TabIndex = 12;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBar1.Value = 50;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(134, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "曲名：";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(448, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 18);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "音量：";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(494, 150);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 30);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "停止播放";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(494, 200);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 33);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "下一曲";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(109, 345);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(319, 46);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip9.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip9;
+        private System.Windows.Forms.ToolStripMenuItem selectNormalFilesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem selectOggFilesToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsMusicPlayer/Form1.cs
@@ -0,0 +1,163 @@
+﻿using NAudio.Vorbis;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsMusicPlayer
+{
+    public partial class Form1 : Form
+    {
+        // 本地音乐列表
+        List<string> localmusiclist = new List<string>();
+        // 用于 OGG 文件播放
+        private WaveOutEvent waveOut = new WaveOutEvent();
+        private VorbisWaveReader currentVorbisReader;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void musicplay(string filename)
+        {
+            string extension = Path.GetExtension(filename).ToLower();
+
+            if (extension == ".ogg")
+            {
+                Console.WriteLine("这是ogg文件。");
+                if (currentVorbisReader != null)
+                {
+                    // 确保先停止当前播放再释放资源
+                    if (waveOut.PlaybackState != PlaybackState.Stopped)
+                    {
+                        waveOut.Stop();
+                    }
+                    currentVorbisReader.Dispose();
+                }
+
+                currentVorbisReader = new VorbisWaveReader(filename);
+                waveOut.Init(currentVorbisReader);
+                waveOut.Play();
+            }
+            else
+            {
+                axWindowsMediaPlayer1.URL = filename;
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+        }
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 设置默认音量
+            trackBar1.Value = 50;
+            axWindowsMediaPlayer1.settings.volume = 50;
+            label2.Text = "50%";
+        }
+
+        private void selectNormalFilesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "选择普通音乐文件|*.mp3;*.flac;*.wav";
+            openFileDialog1.Multiselect = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string file in openFileDialog1.FileNames)
+                {
+                    string fileName = Path.GetFileName(file);
+                    if (!localmusiclist.Contains(file))
+                    {
+                        listBox1.Items.Add(fileName);
+                        localmusiclist.Add(file);
+                    }
+                }
+            }
+        }
+
+        private void selectOggFilesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "选择.ogg音乐文件|*.ogg";
+            openFileDialog1.Multiselect = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string file in openFileDialog1.FileNames)
+                {
+                    string fileName = Path.GetFileName(file);
+                    if (!localmusiclist.Contains(file))
+                    {
+                        listBox1.Items.Add(fileName);
+                        localmusiclist.Add(file);
+                    }
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (localmusiclist.Count > 0 && listBox1.SelectedIndex >= 0)
+            {
+                string selectedFile = localmusiclist[listBox1.SelectedIndex];
+                musicplay(selectedFile);
+                label1.Text = Path.GetFileNameWithoutExtension(selectedFile); // 设置曲目名称
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (waveOut.PlaybackState == PlaybackState.Playing)
+            {
+                waveOut.Pause(); // 暂停播放 OGG
+            }
+
+            axWindowsMediaPlayer1.Ctlcontrols.stop(); // 停止其他格式的播放
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (localmusiclist.Count > 0)
+            {
+                int index = (listBox1.SelectedIndex + 1);
+
+                if (index >= localmusiclist.Count())
+                {
+                    index = 0;
+                }
+
+                string nextFile = localmusiclist[index];
+                musicplay(nextFile);
+                label1.Text = Path.GetFileNameWithoutExtension(nextFile);
+                listBox1.SelectedIndex = index;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            contextMenuStrip9.Show(button1, new System.Drawing.Point(0, button1.Height));
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.settings.volume = trackBar1.Value;
+            label2.Text = trackBar1.Value + "%";
+            if (waveOut != null)
+            {
+                waveOut.Volume = trackBar1.Value / 100f;
+            }
+        }
+    }
+}
